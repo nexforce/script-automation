0b2f405c469e3ee96500479164bd5cb24859d759
Gabriel Flores <gabriel.flores@nexforce.co>
2024-07-15 14:48:21 -0300

    Use batch APIs to handle data

--- hubspot/custom-code/js/Client-Specific-Codes/Metadados/customSolucoesDoClienteAutomation.js
+++ hubspot/custom-code/js/Client-Specific-Codes/Metadados/customSolucoesDoClienteAutomation.js
@@ -1,9 +1,7 @@
-const hubspot = require('@hubspot/api-client');
-
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+const hubspot = require("@hubspot/api-client");
 
 const stringifyOrExtractError = (error) =>
-  error.message === 'HTTP request failed'
+  error.message === "HTTP request failed"
     ? JSON.stringify(error.response, undefined, 2)
     : error.message;
 
@@ -25,13 +23,74 @@ const propertyEquals = (propertyName, value) => ({
       filters: [
         {
           propertyName,
-          operator: 'EQ',
+          operator: "EQ",
           value,
         },
       ],
     },
   ],
 });
+
+const searchExistingSolutionsRequestBody = (productSkus, after) => {
+  let body = {
+    filterGroups: [
+      {
+        filters: [
+          {
+            operator: "IN",
+            propertyName: "c_digo_do_produto",
+            values: productSkus,
+          },
+        ],
+      },
+    ],
+    properties: [
+      "hs_object_id",
+      "c_digo_do_produto",
+      "status_da_solucao_do_cliente",
+    ],
+    limit: 100,
+  };
+
+  if (after) {
+    body = {
+      ...body,
+      ...after,
+    };
+  }
+
+  return body;
+};
+
+const searchProductsRequestBody = (solutionsToCreate, after) => {
+  let body = {
+    filterGroups: [
+      {
+        filters: [
+          {
+            propertyName: "hs_sku",
+            operator: "IN",
+            values: solutionsToCreate.map((sku) => String(sku).toLowerCase()),
+          },
+        ],
+      },
+    ],
+    properties: ["name", "hs_folder_name", "hs_sku", "id_metadados"],
+    limit: 100,
+  };
+
+  if (after) {
+    body = {
+      ...body,
+      ...after,
+    };
+  }
+
+  return body;
+};
+
+const solutionObjectTypeId = "2-29995024";
+const associationTypeId = 290;
 
 exports.main = async (event) => {
   const { API_TOKEN: accessToken } = process.env;
@@ -44,8 +103,8 @@ exports.main = async (event) => {
   try {
     const searchCompaniesResponse =
       await client.crm.companies.searchApi.doSearch({
-        ...propertyEquals('name', companyName),
-        properties: ['hs_object_id'],
+        ...propertyEquals("name", companyName),
+        properties: ["hs_object_id"],
       });
 
     if (searchCompaniesResponse.results.length === 0) {
@@ -59,166 +118,143 @@ exports.main = async (event) => {
     const companyId =
       searchCompaniesResponse.results[0].properties.hs_object_id;
 
+    delete searchCompaniesResponse;
+
     if (!sdc) {
       throwMessage('propriedade "Soluções do Cliente" está vazia');
     }
 
-    const getAllSchemasResponse = await client.crm.schemas.coreApi.getAll(
-      false
-    );
-
-    if (getAllSchemasResponse.results.length === 0) {
-      throwMessage('nenhum schema encontrado');
-    }
-
-    const sdcSchema = getAllSchemasResponse.results.find(
-      (schema) => schema.name === 'solu__es_do_cliente'
-    );
-
-    if (!sdcSchema) {
-      throwMessage(
-        'schema do objeto personalizado "Soluções do Cliente" não encontrado'
-      );
-    }
-
-    const sdcToCompanyAssocResponse =
-      await client.crm.associations.v4.schema.definitionsApi.getAll(
-        sdcSchema.objectTypeId,
-        '0-2'
-      );
-
-    if (sdcToCompanyAssocResponse.results.length === 0) {
-      throwMessage(
-        'nenhuma associação entre o objeto personalizado "Soluções do Cliente" e o objeto "Empresa" foi encontrada, favor criar uma'
-      );
-    }
-
-    const sdcToCompanyAssoc = sdcToCompanyAssocResponse.results.find(
-      (a) => a.label === 'Produto'
-    );
-
-    if (!sdcToCompanyAssoc) {
-      throwMessage(
-        'não foi encontrado um rótulo de associação entre o objeto personalizado "Soluções do Cliente" e o objeto "Empresa", favor criar um rótulo nomeado "Produto"'
-      );
-    }
-
-    const productSkus = sdc.split(';');
+    const productSkus = sdc.split(";");
     console.log(
       `rodando workflow para os seguintes SKUs de produtos: ${productSkus}`
     );
+
     const existingSolutions = [];
-    let solutionsToCreate = [];
-
-    for (const sku of productSkus) {
-      solutionsToCreate.push(sku);
-
-      let existingSolutionResponse;
-      try {
-        existingSolutionResponse = await client.crm.objects.searchApi.doSearch(
-          sdcSchema.objectTypeId,
-          {
-            ...propertyEquals('c_digo_do_produto', sku),
-            properties: [
-              'hs_object_id',
-              'c_digo_do_produto',
-              'status_da_solucao_do_cliente',
-            ],
-          }
+    try {
+      let existingSolutionsResponse =
+        await client.crm.objects.searchApi.doSearch(
+          solutionObjectTypeId,
+          searchExistingSolutionsRequestBody(productSkus)
         );
-      } catch (error) {
-        throwError(
-          error,
-          'ocorreu um erro ao buscar a solução do cliente existente'
+
+      existingSolutionsResponse.results.forEach((r) =>
+        existingSolutions.push(r)
+      );
+
+      while (existingSolutionsResponse.paging?.next?.after !== undefined) {
+        existingSolutionsResponse = await client.crm.objects.searchApi.doSearch(
+          solutionObjectTypeId,
+          searchExistingSolutionsRequestBody(productSkus, {
+            after: Number.parseInt(existingSolutionsResponse.paging.next.after),
+          })
+        );
+
+        existingSolutionsResponse.results.forEach((r) =>
+          existingSolutions.push(r)
         );
       }
-
-      await sleep(200);
-
-      if (existingSolutionResponse.total > 0) {
-        for (const existingSolution of existingSolutionResponse.results) {
-          const sdcAssocResponse =
-            await client.crm.associations.v4.basicApi.getPage(
-              sdcSchema.objectTypeId,
-              existingSolution.id,
-              '0-2'
-            );
-
-          await sleep(200);
-
-          const solutionBelongsToCurrentCompany =
-            !!sdcAssocResponse.results.find(
-              ({ toObjectId }) => String(toObjectId) === companyId
-            );
-
-          if (
-            solutionBelongsToCurrentCompany &&
-            String(existingSolution.properties.status_da_solucao_do_cliente)
-              .trim()
-              .toLowerCase() !== 'inativa'
-          ) {
-            existingSolutions.push(sku);
-          }
-        }
-      }
+    } catch (error) {
+      throwError(
+        error,
+        "Ocorreu um erro ao buscar as soluções do cliente existentes"
+      );
     }
 
-    solutionsToCreate = solutionsToCreate.filter(
-      (sku) => !existingSolutions.find((es) => es === sku)
+    let solutionToCompanyAssociationsResponse;
+    try {
+      solutionToCompanyAssociationsResponse =
+        await client.crm.associations.v4.batchApi.getPage(
+          solutionObjectTypeId,
+          "0-2",
+          {
+            inputs: existingSolutions.map((r) => ({
+              id: r.id,
+            })),
+          }
+        );
+    } catch (error) {
+      throwError(
+        error,
+        "Ocorreu um erro ao buscar as associações entre as soluções existentes e as empresas"
+      );
+    }
+
+    const solutionsNotToCreate = solutionToCompanyAssociationsResponse.results
+      .filter((s) => String(s.to[0].toObjectId) === companyId)
+      .filter((s) => !!existingSolutions.find((es) => es.id === s._from.id))
+      .map((s) => existingSolutions.find((es) => es.id === s._from.id))
+      .filter(
+        (s) =>
+          s.properties.status_da_solucao_do_cliente.trim().toLowerCase() !==
+          "inativa"
+      )
+      .map((s) => s.properties.c_digo_do_produto);
+
+    delete existingSolutions;
+    delete solutionToCompanyAssociationsResponse;
+
+    const solutionsToCreate = productSkus.filter(
+      (sku) => !solutionsNotToCreate.find((es) => !!es && es === sku)
     );
 
     if (solutionsToCreate.length === 0) {
       console.log(
-        'detectei que nenhuma solução precisa ser criada. encerrando por aqui'
+        "detectei que nenhuma solução precisa ser criada. encerrando por aqui"
       );
       return;
     }
 
-    console.log(`as seguintes soluções serão criadas: ${solutionsToCreate}`);
     console.log(
-      `as seguintes soluções NÃO serão criadas: ${existingSolutions}`
+      `as seguintes soluções serão criadas: ${JSON.stringify(
+        solutionsToCreate
+      )}`
+    );
+    console.log(
+      `as seguintes soluções NÃO serão criadas: ${JSON.stringify(
+        solutionsNotToCreate
+      )}`
     );
 
+    const products = [];
     let searchProductsResponse;
     try {
-      searchProductsResponse = await client.crm.products.searchApi.doSearch({
-        filterGroups: [
-          {
-            filters: [
-              {
-                propertyName: 'hs_sku',
-                operator: 'IN',
-                values: solutionsToCreate.map((sku) =>
-                  String(sku).toLowerCase()
-                ),
-              },
-            ],
-          },
-        ],
-        properties: ['name', 'hs_folder_name', 'hs_sku', 'id_metadados'],
-      });
+      searchProductsResponse = await client.crm.products.searchApi.doSearch(
+        searchProductsRequestBody(solutionsToCreate)
+      );
+
+      searchProductsResponse.results.forEach((r) => products.push(r));
+
+      while (searchProductsResponse.paging?.next?.after !== undefined) {
+        searchProductsResponse = await client.crm.products.searchApi.doSearch(
+          searchProductsRequestBody(solutionsToCreate, {
+            after: Number.parseInt(searchProductsResponse.paging.next.after),
+          })
+        );
+
+        searchProductsResponse.results.forEach((r) => products.push(r));
+      }
     } catch (error) {
-      throwError(error, 'ocorreu um erro ao buscar os produtos por SKU');
+      throwError(error, "ocorreu um erro ao buscar os produtos por SKU");
     }
 
-    if (searchProductsResponse.total === 0) {
+    delete searchProductsResponse;
+
+    if (products.length === 0) {
       throwMessage(
         `nenhum produto encontrado com os códigos de SKU: ${solutionsToCreate}`
       );
     }
 
     console.log(
-      `encontrados ${
-        searchProductsResponse.total
-      } produtos na biblioteca: ${searchProductsResponse.results.map(
+      `encontrados ${products.length} produtos na biblioteca: ${products.map(
         (p) => p.properties.hs_sku
       )}`
     );
 
     const createdSolutions = await client.crm.objects.batchApi.create(
-      sdcSchema.objectTypeId,
+      solutionObjectTypeId,
       {
-        inputs: searchProductsResponse.results.map((product) => ({
+        inputs: products.map((product) => ({
           associations: [
             {
               to: {
@@ -226,8 +262,8 @@ exports.main = async (event) => {
               },
               types: [
                 {
-                  associationCategory: 'USER_DEFINED',
-                  associationTypeId: sdcToCompanyAssoc.typeId,
+                  associationCategory: "USER_DEFINED",
+                  associationTypeId,
                 },
               ],
             },
@@ -237,17 +273,17 @@ exports.main = async (event) => {
             c_digo_do_produto: product.properties.id_metadados,
             shs_sku: product.properties.hs_sku,
             nome_da_pasta: product.properties.hs_folder_name,
-            status_da_solucao_do_cliente: 'Ativa',
+            status_da_solucao_do_cliente: "Ativa",
           },
         })),
       }
     );
 
-    console.log('operação de criação terminou');
+    console.log("operação de criação terminou");
     console.log(`status: ${createdSolutions.status}`);
     console.log(`${createdSolutions.results.length} soluções criadas`);
-    console.log(`${createdSolutions.errors} erros`);
+    console.log(`erros: ${createdSolutions.errors || 0}`);
   } catch (error) {
-    stringifyOrExtractError(error);
+    console.error(stringifyOrExtractError(error));
   }
 };
