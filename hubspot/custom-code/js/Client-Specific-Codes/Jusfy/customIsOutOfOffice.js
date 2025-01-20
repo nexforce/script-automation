8de83cbaad2e6f6d4b9408e193eaa110fa642206
lufelipe12 <lufelipe.dias@gmail.com>
2025-01-20 16:32:23 -0300

    feat: added code to verify weekday and hour, returning outOfOffice property

---
+++ hubspot/custom-code/js/Client-Specific-Codes/Jusfy/customIsOutOfOffice.js
@@ -0,0 +1,46 @@
+const dotenv = require("dotenv");
+
+// Utiliza o arquivo .env para obter o accessToken (Importante: nunca mandar o accessToken para o repositório!)
+dotenv.config();
+
+// Ao mandar pro custom code do fluxo, copiar a partir desta linha até o comentário [FINAL]
+
+exports.main = async (event, callback) => {
+  try {
+    const blockedDays = [0, 7];
+
+    const weekDay = new Date().getDay();
+    const hours = new Date().getHours();
+
+    const isOutOfOffice =
+      blockedDays.some((day) => day == weekDay) || hours >= 18 || hours < 9;
+
+    return await callback({
+      outputFields: {
+        isOutOfOffice,
+      },
+    });
+  } catch (err) {
+    await callback({
+      outputFields: { hs_execution_state: "FAIL_CONTINUE" },
+    });
+    console.error(err);
+    // Force retry if error is on cloudflare's side. (https://developers.hubspot.com/docs/api/error-handling#custom-code-workflow-actions)
+    if (axios.isAxiosError(err) && JSON.stringify(err).includes("cloudflare"))
+      err.response.status = 500;
+    // We will automatically retry when the code fails because of a rate limiting error from the HubSpot API.
+    throw err;
+  }
+};
+
+// [FINAL]
+
+exports.main(
+  {
+    inputFields: {},
+    object: {
+      objectId: "",
+    },
+  },
+  console.log
+);
---
+++ hubspot/custom-code/js/Client-Specific-Codes/Jusfy/customSendAfkWppMessage.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv");
+
+// Utiliza o arquivo .env para obter o accessToken (Importante: nunca mandar o accessToken para o repositório!)
+dotenv.config();
+
+// Ao mandar pro custom code do fluxo, copiar a partir desta linha até o comentário [FINAL]
+
+exports.main = async (event, callback) => {
+  try {
+    return await callback({
+      outputFields: {},
+    });
+  } catch (err) {
+    await callback({
+      outputFields: { hs_execution_state: "FAIL_CONTINUE" },
+    });
+    console.error(err);
+    // Force retry if error is on cloudflare's side. (https://developers.hubspot.com/docs/api/error-handling#custom-code-workflow-actions)
+    if (axios.isAxiosError(err) && JSON.stringify(err).includes("cloudflare"))
+      err.response.status = 500;
+    // We will automatically retry when the code fails because of a rate limiting error from the HubSpot API.
+    throw err;
+  }
+};
+
+// [FINAL]
+
+exports.main(
+  {
+    inputFields: {},
+    object: {
+      objectId: "",
+    },
+  },
+  console.log
+);
