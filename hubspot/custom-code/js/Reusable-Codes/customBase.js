5b6000b8b8a36134f6f0293eaf46d40028779303
lufelipe12 <lufelipe.dias@gmail.com>
2025-05-07 13:39:33 -0300

    feat: added new block of code for new custom codes

---
+++ hubspot/custom-code/js/Reusable-Codes/customBase.js
@@ -0,0 +1,48 @@
+const dotenv = require("dotenv");
+
+// Utiliza o arquivo .env para obter o accessToken (Importante: nunca mandar o accessToken para o repositório!)
+dotenv.config();
+
+// Ao mandar pro custom code do fluxo, copiar a partir desta linha até o comentário [FINAL]
+
+async function hubspotCallFunction(id) {
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    const response = await axios({});
+
+    return response.data;
+  } catch (error) {
+    console.error("", error.message);
+    const errorMessage =
+      error.response?.data?.message || "Unkown error on Hubspot.";
+
+    console.error(errorMessage);
+
+    throw new Error(errorMessage);
+  }
+}
+
+exports.main = async (event, callback) => {
+  try {
+  } catch (error) {
+    console.error(err);
+    // Force retry if error is on cloudflare's side. (https://developers.hubspot.com/docs/api/error-handling#custom-code-workflow-actions)
+    if (axios.isAxiosError(err) && JSON.stringify(err).includes("cloudflare"))
+      err.response.status = 500;
+    // We will automatically retry when the code fails because of a rate limiting error from the HubSpot API.
+    throw err;
+  }
+};
+
+// [FINAL];
+
+exports.main(
+  {
+    inputFields: {},
+    object: { objectId: "" },
+  },
+  console.log
+);
